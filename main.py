9b6d67297acdb0914183c54ef28bde3dcdc84b13
Albert Gareev <Albert.Gareev.2018@yandex.ru>
2021-02-22 15:22:11 +0400

    second task

--- main.py
+++ main.py
@@ -12,36 +12,50 @@ SCREEN_SIZE = [800, 450]
 class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-
+        # import design
         self.setupUi(self)
+
+        # create static map button
         self.pushButton.clicked.connect(self.get_image)
 
     def get_image(self):
-
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.doubleSpinBox_longitude.value()}," \
-                      f"{self.doubleSpinBox_latitude.value()}&spn={self.doubleSpinBox_scale.value()},{self.doubleSpinBox_scale.value()}&l=map"
-
+                      f"{self.doubleSpinBox_latitude.value()}&spn={self.doubleSpinBox_scale.value()},{0.0001}&l=map"
+        # get response
         response = requests.get(map_request)
 
-
         if not response:
-            print("Ошибка выполнения запроса:")
+            print("Request execution error: ")
             print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print("Http status:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        # Запишем полученное изображение в файл.
+        # write img to file
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
+
+        # set pixmap to label
         self.pixmap = QPixmap(self.map_file)
-        print(self.pixmap)
         self.image.setPixmap(self.pixmap)
 
-
+    # delete temp file map.png
     def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
+
+    # key pressed (PgUP and PgDown)
+    def keyPressEvent(self, e):
+        # zoom in
+        if e.key() in [QtCore.Qt.Key_PageUp]:
+            self.doubleSpinBox_scale.setValue(self.doubleSpinBox_scale.value() / 2)
+            self.get_image()
+
+        # zoom out
+        elif e.key() in [QtCore.Qt.Key_PageDown]:
+            self.doubleSpinBox_scale.setValue(self.doubleSpinBox_scale.value() * 2)
+            self.get_image()
+        else:
+            super(QMainWindow, self).keyPressEvent(e)
 
 
 if __name__ == '__main__':
